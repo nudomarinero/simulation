3781c5b04c447e49156fd83799e8893fce730239
José Sabater Montes <jsm@iaa.es>
2013-02-21 22:37:56 +0000

    Rewriting after test.

--- compare.py
+++ compare.py
@@ -14,37 +14,35 @@ msname_source = "/data/scratch/montes/simulation/COSMOS_20130219_sim_source.MS"
 
 threshold = 5.
 
-# Indexes of the correlations
-corrind = {"XX":0,"XY":1,"YX":2,"YY":3}
+class MeasurementSet(object):
+    # Indexes of the correlations
+    corrind = {"XX":0,"XY":1,"YX":2,"YY":3}
+    def __init__(self,msname):
+        self.table = tbl.table(msname)
+        self.model_data = self.table.getcol('MODEL_DATA')
+        self.uvw = self.table.getcol('UVW')
+        self.uvdist = numpy.sqrt(self.uvw[:,0]**2+self.uvw[:,1]**2)
+        self.ntimes = len(numpy.unique(self.table.getcol('TIME')))
+        self.nantennas = len(numpy.unique(self.table.getcol('ANTENNA1')))
+        self.nbaselines = self.nantennas**2/2
+        self.nbaselines_cor = self.nantennas*(self.nantennas-1)/2
+        self.uvorder = numpy.argsort(self.uvdist[0:self.nbaselines])
+        shape_model_data = self.model_data.shape
+        self.nchannels = shape_model_data[1]
+        self.ncors = shape_model_data[2]
+        self.amplitude = numpy.zeros((self.nchannels,self.ncors,self.ntimes,self.nbaselines_cor),dtype=float)
+        for i in range(self.nchannels):
+            for j in range(self.ncors):
+                self.amplitude[i,j,:,:] = numpy.reshape(numpy.absolute(self.model_data[:,i,j]),
+                                                        (self.ntimes,self.nbaselines))[:,self.uvorder][:,self.nantennas:]
 
-cyga = tbl.table(msname_cyga)
-#source = tbl.table(msname_source)
 
-model_data_cyga = cyga.getcol('MODEL_DATA')
-#model_data_source = source.getcol('MODEL_DATA')
+if __name__ == "__main__":
+    cyga = MeasurementSet(msname_cyga)
+    #source = MeasurementSet(msname_source)
+    for i in range(cyga.nchannels):
+        n_affectedxx = numpy.count_nonzero(numpy.array((cyga.amplitude[i,0,:,:] >= threshold),dtype = int))
+        n_affectedyy = numpy.count_nonzero(numpy.array((cyga.amplitude[i,3,:,:] >= threshold),dtype = int))
+        total_points = cyga.ntimes*cyga.nbaselines_cor
+        print "Freq: %i XX:%f %% YY: %f %%"%(i,n_affectedxx/total_points,n_affectedyy/total_points)
 
-uvw = cyga.getcol('UVW')
-uvdist = numpy.sqrt(uvw[:,0]**2+uvw[:,1]**2)
-# TODO: Infer 1770
-uvorder = numpy.argsort(uvdist[0:1770])
-
-# TODO: Infer the number of antennas
-amplitude_cyga_xx = numpy.reshape(numpy.absolute(model_data_cyga[:,0,0]), (1800,1770))[:,uvorder][:,59:]
-#amplitude_source_xx = numpy.reshape(numpy.absolute(model_data_source[:,0,0]), (1800,1770))[:,uvorder][:,59:]
-
-amplitude_cyga_yy = numpy.reshape(numpy.absolute(model_data_cyga[:,0,3]), (1800,1770))[:,uvorder][:,59:]
-#amplitude_source_yy = numpy.reshape(numpy.absolute(model_data_source[:,0,3]), (1800,1770))[:,uvorder][:,59:]
-
-#rel_xx = numpy.log10(amplitude_cyga_xx/amplitude_source_xx)
-#rel_yy = numpy.log10(amplitude_cyga_yy/amplitude_source_yy)
-
-nchan = 14
-
-for i in range(nchan):
-    amplitude_cyga_xx = numpy.reshape(numpy.absolute(model_data_cyga[:,i,0]), (1800,1770))[:,uvorder][:,59:]
-    amplitude_cyga_yy = numpy.reshape(numpy.absolute(model_data_cyga[:,i,3]), (1800,1770))[:,uvorder][:,59:]
-
-    n_affectedxx = numpy.count_nonzero(numpy.array((amplitude_cyga_xx >= threshold),dtype = int))
-    n_affectedyy = numpy.count_nonzero(numpy.array((amplitude_cyga_yy >= threshold),dtype = int))
-
-    print "Freq: %i XX:%f %% YY: %f %%"%(i,n_affectedxx/1800./1711.,n_affectedyy/1800./1711.)
