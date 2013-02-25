035d0897f47f1a50277db64cd061af3200382f65
José Sabater Montes <jsm@iaa.es>
2013-02-25 19:21:43 +0000

    Statistics added.

--- compare.py
+++ compare.py
@@ -38,24 +38,11 @@ class SimulationSet(object):
                                                 (self.ntimes, self.nbaselines))[:,self.uvorder][:,self.nantennas:]
 
 
-def threshold(SS,threshold=5.):
-    """
-    computes the fraction of measurements above the threshold for a given correlation.
-    """
-    #TODO: Merge with threshold_axis
-    total_points = float(SS.ntimes*SS.nbaselines_cor)
-    n_affected = numpy.zeros((SS.nchannels,SS.ncors), dtype=float)
-    for i in range(SS.nchannels):
-        for j in range(SS.ncors):
-            n_affected[i,j] = numpy.count_nonzero(numpy.array((SS.amplitude[i,j,:,:] >= threshold), dtype=int))
-    return n_affected/total_points
-
-
 def threshold_axis(SS,threshold=5.,axis=1):
     """
-    computes statistics for the measurements above the threshold for a given correlation
+    Computes statistics for the measurements above the threshold for all the correlations
     with respect to an axis (by default axis 1 or time).
-    It returns the whole measure.
+    It returns a list of all the measures and the total number of points in the bins.
     """
     if axis == 0:
         naxis = SS.nbaselines_cor
@@ -67,12 +54,22 @@ def threshold_axis(SS,threshold=5.,axis=1):
     for i in range(SS.nchannels):
         for j in range(SS.ncors):
             n_affected[i,j,:] = numpy.sum(numpy.array((SS.amplitude[i,j,:,:] >= threshold), dtype = int), axis=axis)
-    return n_affected/total_points
+    return n_affected,total_points
+
+
+def threshold_stats(SS,threshold=5.,axis=1,nfunc="max"):
+    dirfuncs = {"max":numpy.max,"min":numpy.min,"std":numpy.std,
+                "mean":numpy.mean,"median":numpy.median}
+    th_axis,t_points = threshold_axis(SS,threshold=threshold,axis=axis)
+    stats = numpy.zeros((SS.nchannels,SS.ncors),dtype=float)
+    for i in range(SS.nchannels):
+        for j in range(SS.ncors):
+            stats[i,j] = dirfuncs[nfunc](th_axis[i,j,:]/t_points*100.)
 
 
 def compare(SS_A,SS_source,level=0.01):
     """
-    computes the number of measurements where the first source has a value "level" times higher
+    Computes the number of measurements where the first source has a value "level" times higher
     than the second source.
     """
     # Check if the two simulations are compatible
@@ -85,27 +82,30 @@ def compare(SS_A,SS_source,level=0.01):
     n_affected = numpy.zeros((SS_A.nchannels,SS_A.ncors), dtype=float)
     for i in range(SS_A.nchannels):
         for j in range(SS_A.ncors):
-            n_affected[i,j] = numpy.count_nonzero(numpy.array(SS_A.amplitude[i,j,:,:] >=
+            n_affected[i,j] = numpy.count_nonzero(numpy.array(level*SS_A.amplitude[i,j,:,:] >=
                                                               SS_source.amplitude[i,j,:,:], dtype=int))
     return n_affected/total_points
 
 
 #### Output ####
-def print_threshold(a_ss,th):
+def print_threshold(a_ss,th,nfunc="max",nc=2):
     """
     Prints the percentage of affected data (maximum with respect to the time by default)
     for each channel using a threshold in the flux density
     """
-    #TODO: Choose the statistics used
-    per_affected = threshold_axis(a_ss,threshold=th)
+    per_affected = threshold_stats(a_ss,threshold=th,nfunc=nfunc)
     print "==================================="
     print "Flux density threshold: %f"%th
     for i in range(a_ss.nchannels):
-        print "Freq: %2i XX: %6.2f%% YY: %6.2f%%"%(i, numpy.max(per_affected[i,0,:])*100.,
-                                                   numpy.max(per_affected[i,3,:])*100.)
+        if nc == 2:
+            print "Freq: %2i XX: %6.2f%% YY: %6.2f%%"%(i,
+                    per_affected[i,0],per_affected[i,3])
+        else:
+            print "Freq: %2i XX: %6.2f%% XY: %6.2f%% YX: %6.2f%% YY: %6.2f%%"%\
+                  [i].extend([per_affected[i,0] for i in range(a_ss.ncors)])
 
 
-def print_comparison(a_ss,source_ss,level):
+def print_comparison(a_ss,source_ss,level,nc=2):
     """
     Prints the total percentage of affected data for each channel by comparing with
     the source observed.
@@ -115,28 +115,38 @@ def print_comparison(a_ss,source_ss,level):
     print "==================================="
     print "Comparison with source. Level: %f"%level
     for i in range(a_ss.nchannels):
-        print "Freq: %2i XX: %6.2f%% YY: %6.2f%%"%(i, per_compared[i,0]*100., per_compared[i,3]*100.)
+        if nc == 2:
+            print "Freq: %2i XX: %6.2f%% YY: %6.2f%%"%(i, per_compared[i,0]*100., per_compared[i,3]*100.)
+        else:
+            print "Freq: %2i XX: %6.2f%% XY: %6.2f%% YX: %6.2f%% YY: %6.2f%%"% \
+                  [i].extend([per_compared[i,0]*100. for i in range(a_ss.ncors)])
 
 
 def main(args):
     cyga = SimulationSet(args.ateam)
     for t in args.threshold:
-        print_threshold(cyga,t)
+        print_threshold(cyga,t,nfunc=args.stat_function,nc=args.n_correlations)
     if args.source is not None:
         source = SimulationSet(args.source)
         for l in args.level:
-            print_comparison(cyga,source,l)
+            print_comparison(cyga,source,l,nc=args.n_correlations)
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Percentage of data affected by an A-team source')
-    #parser.add_argument('-c','--cache',help='use a local cache to store and retrieve the data',action="store_true")
-    parser.add_argument('ateam',metavar='ateam.MS',help='MS of the source')
+    #parser.add_argument('--cache',help='use a local cache to store and retrieve the data',action="store_true")
+    parser.add_argument('ateam',metavar='ateam.MS',help='MS of the A-Team source '
+                        '(this argument must be placed BEFORE the options -l or -t if they are used)')
     parser.add_argument('-t','--threshold',type=float,default=[5.],nargs='+',help='threshold(s)')
     parser.add_argument('-s','--source',metavar='source.MS',help='MS of target source')
     parser.add_argument('-l','--level',type=float,default=[0.05],nargs='+',
                         help='ratio(s) between the A-team data and source data')
+    parser.add_argument('-f','--stat-function',type=str,default='max',
+                        help='statistical function used for the threshold. It can be chosen between '
+                             '{max,min,median,std,mean}')
+    parser.add_argument('-n','--n-correlations',type=int,default=2,help='correlations considered') # Not used yet
+    #parser.add_argument('-p','--plot',action="store_true",help='produce plots') # Not used yet
 
     #args = parser.parse_args(["-s",msname_source,msname_cyga])
     args = parser.parse_args()
